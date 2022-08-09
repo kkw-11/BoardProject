1a97e7ed070cecfbffa3687710daddc815aba1de
kiwon <kiwon.kim@seeroo.co.kr>
2022-08-09 13:46:59 +0900

    fix: @Data annotation delete

--- src/main/java/com/example/demo/entity/Article.java
+++ src/main/java/com/example/demo/entity/Article.java
@@ -1,18 +1,14 @@
 package com.example.demo.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
+import lombok.*;
 
 import javax.persistence.*;
 
 
 @Entity
-@Data
 @ToString
-@AllArgsConstructor
 @NoArgsConstructor
+@Getter
 public class Article {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
