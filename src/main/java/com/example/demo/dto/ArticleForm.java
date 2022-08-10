33a1cf24423bf14801a8bb3d9b59dfc4bdd1ecf8
kiwon <kiwon.kim@seeroo.co.kr>
2022-08-10 19:52:56 +0900

    feature: ArticleForm DTO field setter method adding

--- src/main/java/com/example/demo/dto/ArticleForm.java
+++ src/main/java/com/example/demo/dto/ArticleForm.java
@@ -11,12 +11,11 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @ToString
 @Getter
-@Setter
+//@Setter
 @NoArgsConstructor
 public class ArticleForm {
 
     @NotNull
-
     private Long id;
     @NotNull
     @Size(min=2, max=30)
@@ -25,6 +24,26 @@ public class ArticleForm {
     private String content;
     @NotNull
     private String author;
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setCreatedDate(LocalDateTime createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public void setModifiedDate(LocalDateTime modifiedDate) {
+        this.modifiedDate = modifiedDate;
+    }
 
     private LocalDateTime createdDate;
     private LocalDateTime modifiedDate;
