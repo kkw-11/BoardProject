f3f2ed3f86a67b1e14e843ea680fc9809dc0bd4e
asus <kkw-11@naver.com>
2022-08-11 22:13:53 +0900

    chore: Moving root of error.html

--- src/main/java/com/example/demo/handler/GlobalExceptionHandler.java
+++ src/main/java/com/example/demo/handler/GlobalExceptionHandler.java
@@ -5,8 +5,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.NestedServletException;
 
-@ControllerAdvice //해당 패키지 어디에선 Exception이 발생하면 해당 클래스로 들어옴
+@ControllerAdvice //해당 패키지 어디에서라도 Exception이 발생하면 해당 클래스로 들어옴
 @Controller
 @Slf4j
 public class GlobalExceptionHandler {
@@ -14,7 +15,7 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(value=Exception.class)
     public String handlerArgumentException(IllegalArgumentException e){
         log.info(e.getMessage());
-        return "error/error";
+        return "error";
     }
 
 }
